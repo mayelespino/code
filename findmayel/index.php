23b352b179d86bc888e2d2ee2723970386d174a2
mayelespino <mayel.espino@gmail.com>
2014-05-11 19:33:37 -0700

    v2

--- findmayel/index.php
+++ findmayel/index.php
@@ -29,7 +29,7 @@
     <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.8.0r4/build/base/base-min.css">
     <style type="text/css" media="screen">
         p { text-align:left; }
-        #map_canvas { width:640px; height:480px; border:1px solid #000; }
+        #map_canvas { width:700px; height:500px; border:1px solid #000; }
     </style>
     <script src="http://maps.google.com/maps?file=api&amp;v=2&amp;key=<?PHP echo $google_maps_key; ?>&amp;sensor=false" type="text/javascript"></script>
     <script type="text/javascript">
@@ -62,7 +62,11 @@
         }
     </script>
 </head>
+
+
+
 <body onload="initialize()" onunload="GUnload()">
+    <h2 align="left">Send me a message:</h2>
     <form action="" method="post">
         <p>
             <label for="msg">Message:</label>
@@ -72,25 +76,26 @@
             <input type="submit" name="btnSend" value="Send" id="btnSend">
         </p>
     </form>
+
+
     <div id="map_canvas"></div>
-    <table>
+    </br>
+    <h2 align="left">Where I am now:</h2>
+    <table width="700">
         <tr>
         <td>Latitude:</td>
         <td><?PHP echo $loc['latitude'];  ?></td>
         <td>Longitude:</td>
         <td><?PHP echo $loc['longitude']; ?></td>
+	<td><?PHP echo date('l jS \of F Y h:i:s A'); ?></>
         </tr>
 	<tr>
 	<td>Address : </td>
-        <td><?PHP echo $json["results"][0]["formatted_address"]; ?></td>
-	</tr>
-    </table>
+        <td colspan="4">
 
   <?PHP
-   $service_url = 'http://maps.google.com/maps/api/geocode/json?latlng='+$loc['latitude']+','+$loc['longitude']+'&sensor=false';
+   //$service_url = 'http://maps.google.com/maps/api/geocode/json?latlng='+$loc['latitude']+','+$loc['longitude']+'&sensor=false';
    $service_url = sprintf('http://maps.google.com/maps/api/geocode/json?latlng=%s,%s&sensor=false',$loc['latitude'] , $loc['longitude']);
-   //$service_url = 'http://maps.google.com/maps/api/geocode/json?latlng=37.154452255962,-121.60248993907&sensor=false';
-   //echo $service_url;
    echo "<br/>";
    $curl = curl_init($service_url);
    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -100,29 +105,39 @@
    curl_close($curl);
    $json = json_decode($curl_response,true);
 
-
-   echo "<br>1=========<br>";
-   echo $json["results"][0]["formatted_address"];
-   echo "<br>2=========<br>";
-   //echo $curl_response;
-   //print_r($json);
-   var_dump($json);
-   echo "<br>3=========<br>";
-foreach ($json as $key => $value) {
-    //echo "$key<br>";
-    foreach ($value as $k => $v) {
-        //echo "$k | $v <br/>";
-	foreach($v as $k2 =>$v2){
-        	echo "$k2 | $v2<br/>";
-		foreach($v2 as $k3 =>$v3){
-        		//echo "$k3 | $v3<br/>";
-		}
-	}
-    }
-}
-   echo "<br>4=========<br>";
-   //curl_close($curl);
+   echo trim($json["results"][0]["formatted_address"]);
 ?>
+	</tr>
+    </table>
 
+
+
+</br>
+<h2  align="left">Last 10 places I've been at:</h2>
+<table width="700">
+<?PHP
+    $file = '5105125323.txt';
+    $line = sprintf("<tr><td>%s</td><td>%s</td></tr>\n", trim($json["results"][0]["formatted_address"]), date('l jS \of F Y h:i:s A'));
+    file_put_contents($file, $line, FILE_APPEND | LOCK_EX);
+    $lines = file($file);
+    if(count($lines) > 10)
+    {
+        unset($lines[0]);
+	$lines = array_values($lines);
+	file_put_contents($file,implode($lines));
+    }
+    $lines = array_reverse($lines);
+    foreach ($lines as $line_num => $line) {
+        echo $line;
+    }
+    fclose($lines);    
+?>
+</table>
 </body>
+<!--
+http://www.phpeveryday.com/articles/PHP-File-Removing-Lines-From-File-P515.html
+http://www.php.net/manual/en/function.ftruncate.php
+http://us3.php.net/count
+http://www.php.net/manual/en/function.array-reverse.php
+-->
 </html>
