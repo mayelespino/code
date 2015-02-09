988e0c3782136a4abe6e2974db007ea8ea0c7d96
mayelespino <mayel.espino@gmail.com>
2015-02-08 16:09:48 -0800

    Print a matrix diagonaly - question from oyaala interview

---
+++ python/printMatrixDiagonal/printMatrixDiagonaly.py
@@ -0,0 +1,97 @@
+__author__ = 'mayelespino'
+
+import random
+
+def generateMatrix(inMatrix):
+    longestRowSize = 0
+    depth = random.randint(1,5)
+    width = random.randint(1,5)
+    for d in xrange(1,depth):
+        newRow = [random.randint(1,100) for r in xrange(width)]
+        inMatrix.append(newRow)
+    depth -= 1
+    return (depth, width)
+
+def printMatrixRegular(inMatrix):
+    if len(inMatrix) < 1:
+        print "nothing to print regular"
+
+    for row in inMatrix:
+        print row
+
+def checkCoordinate(inMatrix, inRow, inCol):
+    validCoordinate =  True
+    try:
+        inMatrix[inRow][inCol]
+    except:
+        validCoordinate = False
+    return validCoordinate
+
+
+
+def printMatrixDiagonal(inMatrix, inDepth, inWidth):
+    if len(inMatrix) < 1:
+        print "nothing to print diagonal"
+
+    if inDepth == 1 or inWidth == 1:
+        for value in inMatrix:
+            print value,
+        return
+
+    totalItems = inDepth * inWidth
+    baseRow = 0
+    baseCol = 1
+    R = 0
+    C = 0
+
+
+    for index in xrange(1,totalItems):
+        try:
+            value = inMatrix[R][C]
+        except:
+            #print ".",
+            if C < (inWidth - 1):
+                baseCol += 1
+                baseRow = 0
+
+            if baseRow >= (inDepth -1):
+                baseRow = 0
+
+            R = baseRow
+            C = baseCol
+            value = inMatrix[R][C]
+
+        print value,
+
+        if R == 0 and C == 0:
+            R = 0
+            C = 1
+        else:
+            R +=1
+            C -=1
+
+        if C == -1:
+            baseCol += 1
+            R = baseRow
+            C = baseCol
+
+        if baseCol >= inWidth:
+            baseRow += 1
+            baseCol = inWidth - 1
+            R = baseRow
+            C = baseCol
+
+    R = inDepth - 1
+    C = inWidth - 1
+    print inMatrix[R][C]
+#
+#
+#
+
+myMatrix = []
+(depth, width) = generateMatrix(myMatrix)
+print "---"
+printMatrixRegular(myMatrix)
+print "---"
+printMatrixDiagonal(myMatrix,depth,width)
+
