add0ae32399a35ee6b67cd1cfab3f6a64b975b74
mayelespino <mayel.espino@gmail.com>
2015-08-16 22:48:04 -0700

    added create list.

--- c/linked-lists/singly-linked.c
+++ c/linked-lists/singly-linked.c
@@ -1,8 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct node_struct {
-    char data[1];
-    node * next;
+    char data;
+    struct node_struct *next;
 } node;
 
 node *head = NULL;
@@ -12,10 +13,26 @@ node* newNode(char inChar)
 {
     node *tmpNode = (node*)malloc(sizeof(node));
     tmpNode->data = inChar;
+    tmpNode->next = inNext;
     return tmpNode;
+}
+
+
+node* createList(char inList[])
+{
+    int Len = sizeof(inList);
+    node *tmpList = newNode(inList[0]);
+    node *currentNode = tmpList;
+    for(int i = 1; i < Len; i++)
+    {
+        currentNode->next = newNode(inList[i]);
+        currentNode = currentNode->next;
+    }
 }
 
 void main(void)
 {
-    head = newNode('a');
+    head = newNode('a',NULL);
+    printf ("char:%c\n",head->data);
+    return;
 }
