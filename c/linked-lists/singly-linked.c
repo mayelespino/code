98d05fe6f95863e74236807ab0203b84b2bb74ae
mayelespino <mayel.espino@gmail.com>
2015-08-16 23:24:22 -0700

    added printList.

--- c/linked-lists/singly-linked.c
+++ c/linked-lists/singly-linked.c
@@ -36,7 +36,7 @@ void printList(node *inList)
     {
         printf("%c,", currentNode->data);
         if (currentNode->next != NULL) {
-            print "---";
+            printf "---";
             currentNode = currentNode->next;
         }
         else
