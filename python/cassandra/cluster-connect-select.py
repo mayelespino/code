4ac4546dac60b6c61b1150fb5e730c9c08b5ce71
Mayel Espino <mayel.espino@gmail.com>
2016-12-07 21:58:28 -0800

    .

--- python/cassandra/cluster-connect-select.py
+++ python/cassandra/cluster-connect-select.py
@@ -1,3 +1,8 @@
 from cassandra.cluster import Cluster
 
 cluster = Cluster()
+session = cluster.connect()
+session.set_keyspace('excelsior')
+rows = session.execute('SELECT * FROM mytable')
+for row in rows:
+    print row
