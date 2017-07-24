8f389f6dda402b5ad0501f9b4795265ec0bbff0a
Mayel Espino <mayel.espino@gmail.com>
2017-07-23 22:44:00 -0700

    initial.

---
+++ python/countTheWords/mergeFiles.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm merged*.json
+
+python3 mergeDictionaries.py m0 big.txt-?.json big.txt-??.json
+for (( i=1; i <= 43; i++ ))
+do
+  python3 mergeDictionaries.py m${i} big.txt-${i}??.json
+done
+echo "now merging the merge files."
+python3 mergeDictionaries.py GrandTotal merged-m*.json
+echo "done"
---
+++ python/countTheWords/processFiles.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+for f in ${1}-* ; do python3 countTheWords.py "$f" `cat $f`; done
