8d7ec277e6698802039b89964080a4dd1d7cf520
Mayel Espino <mayel.espino@gmail.com>
2017-10-18 21:20:01 -0700

    added rust

---
+++ rust/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/root/code/rust/hello_cargo/target/debug/hello_cargo: /root/code/rust/hello_cargo/src/main.rs
---
+++ rust/hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/root/code/rust/hello_cargo/target/release/hello_cargo: /root/code/rust/hello_cargo/src/main.rs
