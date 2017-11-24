fe3f2696f2c6c81edcdd431a80acfb0bdf4c7b5a
Mayel Espino <mayel.espino@gmail.com>
2017-11-23 23:22:20 -0800

    update

--- rust/t03/src/main.rs
+++ rust/t03/src/main.rs
@@ -1,16 +1,25 @@
-use std::fs::File;
+use std::cmp::PartialOrd;
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> &T {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    &largest
+}
 
 fn main() {
-//    panic!("crash and burn");
+    let number_list = vec![34, 50, 25, 100, 65];
 
-//    let v = vec![1, 2, 3];
-//    v[100];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
 
-    let f = File::open("hello.txt");
-    let f = match f {
-        Ok(file) => file,
-        Err(error) => {
-            panic!("There was a problem opening the file: {:?}", error)
-        },
-    };
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 }
---
+++ rust/t03/target/debug/deps/t03-9ad3813f096bc0df.d
@@ -0,0 +1,5 @@
+/Users/mayelespino/Repos/code/rust/t03/target/debug/deps/t03-9ad3813f096bc0df: src/main.rs
+
+/Users/mayelespino/Repos/code/rust/t03/target/debug/deps/t03-9ad3813f096bc0df.d: src/main.rs
+
+src/main.rs:
