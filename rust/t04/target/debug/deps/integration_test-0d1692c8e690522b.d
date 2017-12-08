0d7593794d56923ed904564eca4fe7a1d910c980
Mayel Espino <mayel.espino@gmail.com>
2017-12-07 22:25:21 -0800

    update

---
+++ rust/t04/target/debug/deps/integration_test-0d1692c8e690522b.d
@@ -0,0 +1,5 @@
+/root/code/rust/t04/target/debug/deps/integration_test-0d1692c8e690522b: tests/integration_test.rs
+
+/root/code/rust/t04/target/debug/deps/integration_test-0d1692c8e690522b.d: tests/integration_test.rs
+
+tests/integration_test.rs:
