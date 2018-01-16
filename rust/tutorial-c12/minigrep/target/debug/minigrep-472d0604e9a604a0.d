9e2af5a8cc579fcb373ff1ef0c38a23579371ebe
Mayel Espino <mayel.espino@gmail.com>
2018-01-15 21:37:52 -0800

    -

---
+++ rust/tutorial-c12/minigrep/target/debug/minigrep-472d0604e9a604a0.d
@@ -0,0 +1,1 @@
+/root/code/rust/tutorial-c12/minigrep/target/debug/minigrep-472d0604e9a604a0: /root/code/rust/tutorial-c12/minigrep/src/main.rs /root/code/rust/tutorial-c12/minigrep/src/lib.rs
---
+++ rust/tutorial-c13/simulated_expensive_calculation/src/main.rs
@@ -0,0 +1,15 @@
+use std::thread;
+use std::time::Duration;
+
+fn simulated_expensive_calculation(intensity: u32) -> u32 {
+    println!("calculating slowly...");
+    thread::sleep(Duration::from_secs(2));
+    intensity
+}
+
+
+fn main() {
+use std::thread;
+use std::time::Duration;
+
+}
