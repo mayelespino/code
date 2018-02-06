1380f8fe960fc97e3dbe7d6df334b34e4c49fa83
Mayel Espino <mayel.espino@gmail.com>
2018-02-05 21:05:01 -0800

    _

--- kotlin/binaryTree/src/istreeBalanced.kt
+++ kotlin/binaryTree/src/istreeBalanced.kt
@@ -7,33 +7,44 @@ package code
 import java.util.*
 
 // Node data class
-data class Node(var int_value: Int, var right: Node?, var left: Node?)
-
-
-fun add_node(current:Node, int_add: Int) {
-    //var current: Node = root
-
-    if (int_add > current.int_value && current.right != null) {
-        current.right?.let {
-            add_node(current.right, int_add)
+data class Node(val int_value: Int, var right: Node? = null, var left: Node? = null){
+    fun add(int_to_add: Int){
+        if (int_to_add >= int_value) {
+            if(right != null) {
+                right?.add(int_to_add)
+            } else {
+                var new_node = Node(int_to_add, null, null)
+                right = new_node
+            }
+        } else {
+            if(left != null) {
+                left?.add(int_to_add)
+            } else {
+                var new_node = Node(int_to_add, null, null)
+                left = new_node
+            }
         }
-    } else if (int_add > current.int_value && current.right == null) {
-        //var new_node = Node(int_add, null, null)
-        current.right = Node(int_add, null, null)
     }
+    override fun toString() = " $int_value"
+    fun preOrder()  { print(this); left?.preOrder(); right?.preOrder() }
+    fun inorder()   { left?.inorder(); print(this); right?.inorder() }
+    fun postOrder() { left?.postOrder(); right?.postOrder(); print(this) }
 }
 
-fun add_nodes_random(root:Node)
-{
+fun main(args: Array<String>){
+
+
+    var root = Node(5, null, null)
 
     for(i in 1..20) {
         var next_int = Random().nextInt(99)
-        add_node(root, next_int)
+        root.add(next_int)
     }
 
-}
-fun main(args: Array<String>){
-    var root = Node(5, null, null)
-
-    add_nodes_random(root)
+    println("pre order")
+    root.preOrder()
+    println("\n in order")
+    root.inorder()
+    println("\n post order")
+    root.postOrder()
 }
