0ec47b207809bbf19f7c208fc1c45685f6202b6f
Mayel Espino <mayel.espino@gmail.com>
2020-03-04 10:40:02 -0800

    initial version of elevator.go

--- _elevator_sym/go/elevator.go
+++ _elevator_sym/go/elevator.go
@@ -2,33 +2,50 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
 )
 
 func elevator(id int, floorChan chan int) {
 	for {
 		floor := <-floorChan
-		fmt.Println(id, floor)
+		floorString := strings.Repeat("-", floor)
+		fmt.Println(id, floorString, floor)
 	}
 }
 
 func main() {
-	fc1 := make(chan int)
-	fc2 := make(chan int)
-	go elevator(1, fc1)
-	go elevator(2, fc2)
-	fc1 <- 2
-	fc2 <- 1
+	var fc [5]chan int
+	for i := range fc {
+		fc[i] = make(chan int)
+		go elevator(i, fc[i])
+	}
+
+	maxElevators := len(fc) - 1
 	var floorNum int
+	var elevatorNum int
+
+	for i := range fc {
+		fc[i] <- 1
+	}
+
+	time.Sleep(time.Second * 1)
 
 	for {
+		fmt.Println("Elevator Number: ")
+		fmt.Scanf("%d", &elevatorNum)
+		if elevatorNum == -1 {
+			break
+		} else if elevatorNum > maxElevators {
+			elevatorNum = maxElevators
+		}
+
+		fmt.Println("Floor Number: ")
 		fmt.Scanf("%d", &floorNum)
-		fc1 <- floorNum
-		fc2 <- floorNum
 		if floorNum == 0 {
 			break
 		}
+		fc[elevatorNum] <- floorNum
 	}
-	time.Sleep(time.Millisecond * 1000)
 	fmt.Println("done")
 }
