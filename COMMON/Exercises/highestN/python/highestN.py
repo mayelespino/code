2be07e02b5be0dd8f1993ad78d8fd646f75d159f
Mayel Espino <mayel.espino@gmail.com>
2021-10-24 19:03:13 -0700

    reorg

---
+++ COMMON/Exercises/highestN/python/highestN.py
@@ -0,0 +1,22 @@
+import heapq
+from numpy.random import seed
+from numpy.random import randint
+# seed random number generator
+seed(1)
+# generate some integers
+
+def NLargest(inList, n):
+    result = sorted(list(heapq.nlargest(n,inList)))
+    return(result[0])
+
+
+print(NLargest([1,2,3,4,5,6,7,8,9,0],3))
+
+print(NLargest([i for i in range(20,11500)],3))
+
+print(NLargest(randint(0, 10, 20),2))
+
+print(NLargest(randint(0, 1000000, 1000000),99))
+# References
+# - https://www.geeksforgeeks.org/heap-queue-or-heapq-in-python/
+# - https://machinelearningmastery.com/how-to-generate-random-numbers-in-python/
--- COMMON/rest_calls/c++/readme.md
+++
@@ -1,5 +0,0 @@
-# C++ Rest API
-
-## Librarires
-- [boost](https://www.boost.org/doc/libs/1_38_0/doc/html/boost_asio/examples.html)
--[MS CppREST](http://www.atakansarioglu.com/easy-quick-start-cplusplus-rest-client-example-cpprest-tutorial/)
