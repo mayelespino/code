5e03bc713246b880386b14f813055c982676f6ec
Mayel Espino <mayel.espino@gmail.com>
2021-12-06 20:46:54 -0800

    added mergeSortThreaded.go

---
+++ COMMON/Sorting/mergeSortThreaded.go
@@ -0,0 +1,30 @@
+package main
+
+
+import (
+    "fmt"
+    "time"
+)
+
+
+// a very simple function that we'll
+// make asynchronous later on
+func compute(value int) {
+    for i := 0; i < value; i++ {
+        time.Sleep(time.Second)
+        fmt.Println(i)
+    }
+}
+
+func main() {
+    fmt.Println("Goroutine Tutorial")
+
+    // sequential execution of our compute function
+    compute(10)
+    compute(10)
+
+    // we scan fmt for input and print that to our console
+    var input string
+    fmt.Scanln(&input)
+
+}
