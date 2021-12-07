5b13b731d815faeefffd018446423f9940f69dbc
Mayel Espino <mayel.espino@gmail.com>
2021-12-06 21:05:01 -0800

    mergeSortThreaded.go

--- COMMON/Sorting/mergeSortThreaded.go
+++ COMMON/Sorting/mergeSortThreaded.go
@@ -1,56 +1,40 @@
-// package main
-
-
-// import (
-//     "fmt"
-//     "time"
-// )
-
-
-// // a very simple function that we'll
-// // make asynchronous later on
-// func compute(value int) {
-//     for i := 0; i < value; i++ {
-//         time.Sleep(time.Second)
-//         fmt.Println(i)
-//     }
-// }
-
-// func main() {
-//     fmt.Println("Goroutine Tutorial")
-
-//     // sequential execution of our compute function
-//     go compute(10)
-//     go compute(10)
-
-//     // we scan fmt for input and print that to our console
-//     var input string
-//     fmt.Scanln(&input)
-
-// }
-
-
 package main
+
 
 import (
     "fmt"
-    "math/rand"
+    "time"
 )
 
-func CalculateValue(values chan int) {
+
+// a very simple function that we'll
+// make asynchronous later on
+func compute(value int, values chan int) {
     value := rand.Intn(10)
     fmt.Println("Calculated Random Value: {}", value)
+    for i := 0; i < value; i++ {
+        time.Sleep(time.Second)
+        fmt.Println(i)
+    }
     values <- value
 }
 
 func main() {
-    fmt.Println("Go Channel Tutorial")
 
-  values := make(chan int)
-  defer close(values)
-
-    go CalculateValue(values)
+    fmt.Println("Goroutine Tutorial")
+    values := make(chan int)
+    defer close(values)
+  
+    // sequential execution of our compute function
+    go compute(10)
+    go compute(10)
 
     value := <-values
     fmt.Println(value)
+
+    // we scan fmt for input and print that to our console
+    var input string
+    fmt.Scanln(&input)
+
 }
+
