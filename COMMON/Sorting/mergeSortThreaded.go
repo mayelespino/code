555d0cfbe362ba48dea63a370859fb063fad3d43
Mayel Espino <mayel.espino@gmail.com>
2021-12-10 21:09:56 -0800

    mergeSortThreaded.go

--- COMMON/Sorting/mergeSortThreaded.go
+++ COMMON/Sorting/mergeSortThreaded.go
@@ -18,10 +18,10 @@ func populateArray(arrayLen int) []int {
 }
 
 // 
-func compute(value int, values chan int) {
-    for i := 0; i < 5; i++ {
-        time.Sleep(time.Second)
-        values <- rand.Intn(value)
+func compute(anArray[] int, values chan int) {
+    for i := 0; i < 10; i++ {
+        //time.Sleep(time.Second)
+        values <- rand.Intn(anArray[i])
     }
     close(values)    
 }
@@ -29,22 +29,28 @@ func compute(value int, values chan int) {
 func main() {
 
     fmt.Println("Goroutine Tutorial: 2 ")
+
+    anArray := populateArray(10)
+	fmt.Println(anArray)
+	//anArray = mergeSort(anArray)
+	//fmt.Println(anArray)
+
     right_values := make(chan int, 1)
     //defer close(right_values)
-    left_values := make(chan int, 1)
+    //left_values := make(chan int, 1)
     //defer close(left_values)
   
     // sequential execution of our compute function
-    go compute(9, right_values)
-    go compute(90, left_values)
+    go compute(anArray, right_values)
+    //go compute(90, left_values)
 
     for value:= range right_values {
         fmt.Println("Right value: ", value)
     }
   
-    for value:= range left_values {
-        fmt.Println("Left value: ", value)
-    }
+    //for value:= range left_values {
+    //    fmt.Println("Left value: ", value)
+    //}
     //close(right_values)
     //close(left_values)
 
