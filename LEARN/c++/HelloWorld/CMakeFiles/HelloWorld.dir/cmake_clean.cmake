cc626dd5a4fe6121e644b0e1732716f4bf16e78c
mayel <mayel.espino@gmail.com>
2024-07-29 17:36:28 -0700

    HelloWorld.cpp

---
+++ LEARN/c++/HelloWorld/CMakeFiles/HelloWorld.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloWorld.dir/src/Main.cpp.o"
+  "HelloWorld"
+  "HelloWorld.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
