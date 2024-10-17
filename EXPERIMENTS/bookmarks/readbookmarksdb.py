4cfc2a36d56ce7032c77cacb483e97fed2a17f54
Mayel Espino <mayel.espino@gmail.com>
2024-10-17 12:43:12 -0700

    moved to learn_rnd.

--- EXPERIMENTS/bookmarks/readbookmarksdb.py
+++
@@ -1,9 +0,0 @@
-import sys
-import sqlite3 
-
-conn = sqlite3.connect('bookmarks.db') 
-c = conn.cursor()
-
-c.execute('''SELECT * FROM bookmarks''') 
-for row in c.fetchall(): 
-    print(row)
--- EXPERIMENTS/saltStack/salt-modules/_states/sone.py
+++
@@ -1,6 +0,0 @@
-import salt
-
-def printargs(*args, **kwargs):
-    return(__salt__['eone.printargs'] (*args, **kwargs))
-def hi():
-    return(__salt__['eone.hello']
